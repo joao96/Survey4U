5e69a5f6b83b2caed5d69f8e81720f4d14402512
joao96 <jvpoletti@gmail.com>
2021-12-13 12:41:36 -0300

    🥅 adding custom errors and middleware

--- src/controllers/SendMailController.ts
+++ src/controllers/SendMailController.ts
@@ -1,6 +1,7 @@
 import { Request, Response } from 'express';
 import { resolve } from 'path';
 import { getCustomRepository } from 'typeorm';
+import { AppError } from '../errors/AppError';
 import { SurveysRepository } from '../repositories/SurveysRepository';
 import { SurveysUsersRepository } from '../repositories/SurveysUsersRepository';
 import { UsersRepository } from '../repositories/UsersRepository';
@@ -17,7 +18,7 @@ class SendMailController {
     const user = await usersRepository.findOne({ email });
 
     if (!user) {
-      response.status(400).json({ error: 'User does not exists!' });
+      throw new AppError('User does not exists!');
     }
 
     const survey = await surveyRepository.findOne({
@@ -25,7 +26,7 @@ class SendMailController {
     });
 
     if (!survey) {
-      response.status(400).json({ error: 'Survey does not exists!' });
+      throw new AppError('Survey does not exists!');
     }
 
     const npsPath = resolve(__dirname, '..', 'views', 'emails', 'npsMail.hbs');
